91b8cc6df36133e6f337135383b75fd593a159b1
ryanjanelli <ryanjanelli@gmail.com>
2020-02-26 13:55:34 -0500

    set up Person component to accept props

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <Person />
+        <Person name="Ryan" age="28"/>
+        <Person name="Mike" age="29"> My Hobbies: Rock Climbing</Person>
+        <Person name="Sean" age="30"/>
       </div>
     );
     // the below code is equivalent to the above code. Clearly less efficient. 
