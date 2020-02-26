e74aed53b4bfc4ce9d982089f6c3b52f1dba7ec5
ryanjanelli <ryanjanelli@gmail.com>
2020-02-26 13:48:08 -0500

    added Person component and inserted into App.js

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <Person />
       </div>
     );
+    // the below code is equivalent to the above code. Clearly less efficient. 
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, `Is this working now?`))
   }
 }
 
