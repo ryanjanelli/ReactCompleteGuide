d57f1e31dd75d712740aa7cc00a91f001893eb53
ryanjanelli <ryanjanelli@gmail.com>
2020-02-27 09:06:52 -0500

    reverted back to class based. set up variable in switchNameHandler to be referenced

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -9,32 +9,46 @@ class App extends Component {
       { name: 'Mike', age: 29 },
       { name: 'Sean', age: 30 }
     ]
-  }
+  };
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('Was clicked');
     // DO NOT CHANGE STATE LIKE THIS --- this.state.persons[0].name = 'Ryebreadington' ---
     this.setState({
       persons: [
-        { name: 'Ryebreadington', age: 28 },
+        { name: newName, age: 28 },
         { name: 'Michael', age: 29 },
         { name: 'Seannifer', age: 30 }
       ]
     });
-  }
+  };
 
   render() {
     return (
-      <div className="App">
+      <div className='App'>
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My Hobbies: Rock Climbing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        {/* the below onClick is less efficient than the .bind(this, arg) technique */}
+        <button onClick={() => this.switchNameHandler('Ryebreadington')}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          clickRef={this.switchNameHandler.bind(this, 'Michaelalala')}
+        >
+          {' '}
+          My Hobbies: Rock Climbing
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
       </div>
     );
-    // the below code is equivalent to the above code. Clearly less efficient. 
+    // the below code is equivalent to the above code. Clearly less efficient.
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, `Is this working now?`))
   }
 }
