5620b35819abc44c5233809e481a37fd1a9fbc7d
ryanjanelli <ryanjanelli@gmail.com>
2020-02-27 08:19:20 -0500

    Set up button to change names of persons in state object using setState()

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -10,12 +10,25 @@ class App extends Component {
       { name: 'Sean', age: 30 }
     ]
   }
+
+  switchNameHandler = () => {
+    // console.log('Was clicked');
+    // DO NOT CHANGE STATE LIKE THIS --- this.state.persons[0].name = 'Ryebreadington' ---
+    this.setState({
+      persons: [
+        { name: 'Ryebreadington', age: 28 },
+        { name: 'Michael', age: 29 },
+        { name: 'Seannifer', age: 30 }
+      ]
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My Hobbies: Rock Climbing</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
