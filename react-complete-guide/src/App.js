a119d5d4c03a7731a21b36789af56597ab0f5d67
ryanjanelli <ryanjanelli@gmail.com>
2020-03-10 16:19:13 -0400

    set up nameChangedHandler to update as input field changes

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -13,13 +13,25 @@ class App extends Component {
     showPersons: false
   };
 
-  nameChangedHandler = event => {
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    // less modern approach that is the same as above
+    // const person = Object.assign({}, this.state.persons[personIndex])
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
     this.setState({
-      persons: [
-        { name: 'Ryan', age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: 'Sean', age: 30 }
-      ]
+      persons: persons
     });
   };
 
@@ -59,6 +71,7 @@ class App extends Component {
                 name={person.name}
                 age={person.age}
                 key={person.id}
+                changed={(event) => this.nameChangedHandler(event, person.id)}
               />
             );
           })}
