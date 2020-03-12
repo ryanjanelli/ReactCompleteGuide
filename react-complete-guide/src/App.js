0d5e1342cded35627abb4a0674d400326d1f8eb8
ryanjanelli <ryanjanelli@gmail.com>
2020-03-12 14:20:58 -0400

    dynamically add classes

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -81,10 +81,18 @@ class App extends Component {
       buttonStyle.backgroundColor = 'red';
     }
 
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red'); // classes = ['red']
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold'); // classes = ['red', 'bold']
+    }
+
     return (
       <div className='App'>
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(' ')}>This is really working!</p>
         {/* the below onClick is less efficient than the .bind(this, arg) technique */}
         <button style={buttonStyle} onClick={this.togglePersonsHandler}>
           Toggle Persons
