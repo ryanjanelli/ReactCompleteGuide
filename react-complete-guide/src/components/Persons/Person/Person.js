ae5adfbe670a7c1b8a65a3a8a5872b1e4bf4ae80
ryanjanelli <ryanjanelli@gmail.com>
2020-03-17 14:31:12 -0400

    restructured program where each component has a clear job

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -2,17 +2,13 @@ import React from 'react';
 import classes from './Person.css';
 
 const person = props => {
-  const rnd = Math.random();
-  if (rnd > 0.7) {
-    throw new Error('Something went wrong');
-  }
   return (
     <div className={classes.Person}>
-      <p onClick={props.click}>
+      <p onClick={props.onClick}>
         I'm {props.name} and I am {props.age}!
       </p>
       <p>{props.children}</p>
-      <input type='text' onChange={props.changed} value={props.name} />
+      <input type='text' onChange={props.onChange} value={props.name} />
     </div>
   );
 };
--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
-import Person from './Person/Person';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
   state = {
@@ -10,8 +11,7 @@ class App extends Component {
       { id: 'zxc1', name: 'Sean', age: 30 }
     ],
     derp: 'derp',
-    showPersons: false,
-    smallChange: 'one two three'
+    showPersons: false
   };
 
   nameChangedHandler = (event, id) => {
@@ -53,46 +53,24 @@ class App extends Component {
 
   render() {
     let persons = null;
-    let btnClass = '';
 
     if (this.state.showPersons) {
       persons = (
-        <div>
-          {this.state.persons.map((person, index) => {
-            return (
-              <Person 
-                click={() => this.deletePersonHandler(index)}
-                name={person.name}
-                age={person.age}
-                key={person.id}
-                changed={event => this.nameChangedHandler(event, person.id)}
-                />
-            );
-          })}
-        </div>
+          <Persons 
+          persons={this.state.persons}
+          clicked={this.deletePersonHandler}
+          changed={this.nameChangedHandler}
+          />
       );
-      btnClass = classes.Red;
-    }
-
-    const assignedClasses = [];
-    if (this.state.persons.length <= 2) {
-      assignedClasses.push(classes.red); // assignedClasses = ['red']
-    }
-    if (this.state.persons.length <= 1) {
-      assignedClasses.push(classes.bold); // assignedClasses = ['red', 'bold']
     }
 
     return (
       <div className={classes.App}>
-        <h1>Hi, I'm a React App</h1>
-        <p className={assignedClasses.join(' ')}>This is really working!</p>
-        {/* the below onClick is less efficient than the .bind(this, arg) technique */}
-        <button
-          className={btnClass}
-          onClick={this.togglePersonsHandler}
-        >
-          Toggle Persons
-        </button>
+        <Cockpit 
+        persons={this.state.persons}
+        showPersons={this.state.showPersons}
+        clicked={this.togglePersonsHandler}
+        />
         {persons}
       </div>
     );
