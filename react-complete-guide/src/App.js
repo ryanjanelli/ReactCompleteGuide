d19677380804c7f07d89955d8fe648413929d55e
ryanjanelli <ryanjanelli@gmail.com>
2020-03-17 11:25:18 -0400

    removed ErrorBoundary from App.js

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Person from './Person/Person';
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
   state = {
@@ -11,7 +10,8 @@ class App extends Component {
       { id: 'zxc1', name: 'Sean', age: 30 }
     ],
     derp: 'derp',
-    showPersons: false
+    showPersons: false,
+    smallChange: 'one two three'
   };
 
   nameChangedHandler = (event, id) => {
@@ -60,15 +60,13 @@ class App extends Component {
         <div>
           {this.state.persons.map((person, index) => {
             return (
-              <ErrorBoundary key={person.id}>
-              <Person
+              <Person 
                 click={() => this.deletePersonHandler(index)}
                 name={person.name}
                 age={person.age}
-                
+                key={person.id}
                 changed={event => this.nameChangedHandler(event, person.id)}
                 />
-                </ErrorBoundary>
             );
           })}
         </div>
