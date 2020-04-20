e292e4fbcba686a5ab4e3bc692e4c082038b5766
ryanjanelli <ryanjanelli@gmail.com>
2020-04-20 10:01:39 -0400

    dynamically linked a posts var to the data from get request

--- reaching-out-to-the-web/src/containers/Blog/Blog.js
+++ reaching-out-to-the-web/src/containers/Blog/Blog.js
@@ -7,31 +7,33 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+  state = {
+    posts: [],
+  };
 
-    componentDidMount() {
-        const posts = axios.get('https://jsonplaceholder.typicode.com/posts')
-        .then(response => {
-            console.log(response);
-        });
-    }
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts').then((response) => {
+      this.setState({ posts: response.data });
+    //   console.log(response);
+    });
+  }
 
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
-    }
+  render() {
+    const posts = this.state.posts.map((post) => {
+      return <Post key={post.id} title={post.title} />;
+    });
+    return (
+      <div>
+        <section className='Posts'>{posts}</section>
+        <section>
+          <FullPost />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
